162526d387d22a32bffb82e231aff295acc8c120
IshiS98 <ishankasandamali98@gmail.com>
2021-06-15 15:37:35 +0530

    Updates

---
+++ precache-manifest.de86737695065152dcc5945bcb665285.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9341901c7c47244433ef21de77f26394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e256598d86549639ec6e",
+    "url": "/static/css/main.a90790e1.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e256598d86549639ec6e",
+    "url": "/static/js/main.e5c3b59c.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "35c80580fa9747f08fbd760be8bdff02",
+    "url": "/static/media/ishankasandamali.35c80580.jpg"
+  },
+  {
+    "revision": "bc3186c53afcd2ad854a51d150e9ab28",
+    "url": "/static/media/resume.bc3186c5.pdf"
+  }
+]);
